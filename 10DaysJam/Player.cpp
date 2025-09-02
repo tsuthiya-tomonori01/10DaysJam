759c7f14fe3b62e780c2c75c860e5e039b3a1a00
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2025-09-02 16:45:25 +0900

    プレイヤーの移動処理と生存フラグを追加

--- 10DaysJam/Player.cpp
+++ 10DaysJam/Player.cpp
@@ -4,12 +4,30 @@ using namespace KamataEngine;
 
 void Player::Start()
 {
+	worldTransform_.Initialize();
+
+	PlayerAlive_ = false;
 }
 
 void Player::Update()
 {
+	if (PlayerAlive_ == true)
+	{
+		//プレイヤー・移動
+		if (Input::GetInstance()->PushKey(DIK_D))
+		{
+			worldTransform_.translation_.x += 10;
+		}
+		else if (Input::GetInstance()->PushKey(DIK_A))
+		{
+			worldTransform_.translation_.x -= 10;
+		}
+
+		//プレイヤー・ジャンプ
+	}
 }
 
 void Player::Draw()
 {
+	
 }
